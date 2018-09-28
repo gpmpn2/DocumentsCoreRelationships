a725ceb38009acd7be22cbc8aafe0df0bd69994c
Grant Maloney <grantmaloney@mwc-035196.dhcp.missouri.edu>
2018-09-28 11:19:04 -0500

    Initial commit

---
+++ DocumentsCoreRelationships/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DocumentsCoreRelationships
+//
+//  Created by Grant Maloney on 9/28/18.
+//  Copyright © 2018 Grant Maloney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
